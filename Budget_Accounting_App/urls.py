13a494ca11b2bf004d540dd7b97b775b98a97302
Mateusz-11 <tweak.night@gmail.com>
2022-11-29 18:17:03 +0100

    Added Home View

--- Budget_Accounting_App/urls.py
+++ Budget_Accounting_App/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from budget_app.views import HomeView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name="home-view"),
 ]
--- budget_app/views.py
+++ budget_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'budget_app/home_view.html')
