d032d42d7baa9a3503594daabaea797e20023891
Mateusz-11 <tweak.night@gmail.com>
2022-11-29 20:06:53 +0100

    Added CategoryView + Bootstrap

--- Budget_Accounting_App/urls.py
+++ Budget_Accounting_App/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from budget_app.views import HomeView
+from budget_app.views import HomeView, CategoryView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name="home-view"),
+    path('category/', CategoryView.as_view(), name="category-view"),
 ]
--- budget_app/views.py
+++ budget_app/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.views import View
 
+from budget_app.models import Category
+
 
 # Create your views here.
 class HomeView(View):
     def get(self, request):
         return render(request, 'budget_app/home_view.html')
+
+
+class CategoryView(View):
+    def get(self, request):
+        cat = Category.objects.all()
+        ctx = {
+            'categories': cat,
+        }
+        return render(request, 'budget_app/categories_view.html', ctx)
