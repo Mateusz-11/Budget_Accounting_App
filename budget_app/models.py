f34d3520d92c04d7b2172686c6ebca12d08fe138
Mateusz-11 <tweak.night@gmail.com>
2022-12-01 21:30:21 +0100

    Changed Models

--- budget_app/models.py
+++ budget_app/models.py
@@ -13,19 +13,19 @@ class Contractors(models.Model):
     def __str__(self):
         return self.contractor_name
 
-
-class Invoice(models.Model):
-    nr_invoice = models.CharField(max_length=64)
-    date_of_issue = models.DateField(null=True)
-    id_contractor = models.ForeignKey(Contractors, on_delete=models.CASCADE)
-
-
 class Category(models.Model):
     id_category = models.BigAutoField(primary_key=True)
     category_name = models.CharField(max_length=32)
 
     def __str__(self):
         return self.category_name
+
+
+class Invoice(models.Model):
+    nr_invoice = models.CharField(max_length=64)
+    date_of_issue = models.DateField(null=True)
+    id_contractor = models.ForeignKey(Contractors, on_delete=models.CASCADE)
+    category_name = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
 class Service(models.Model):
@@ -36,12 +36,20 @@ class Service(models.Model):
     id_category = models.ManyToManyField(Category)
 
 
+class PartialBudget(models.Model):
+    category_name = models.CharField(max_length=32)
+    plan_amount_category = models.IntegerField()
+    execution_amount_category = models.IntegerField()
+
+
 class Budget(models.Model):
     id_budget = models.BigAutoField(primary_key=True)
     budget_name = models.IntegerField(default=2000)
     plan = models.IntegerField(default=0)
     execution = models.IntegerField(default=0)
     id_category = models.ManyToManyField(Category)
+    categories_budget = models.ManyToManyField(PartialBudget)
 
     def __str__(self):
         return str(self.budget_name)
+
