aba51430191d035b066c5f96470c431536019557
Mateusz-11 <tweak.night@gmail.com>
2022-12-13 21:39:37 +0100

    Updated test forms

--- budget_app/tests/test_forms.py
+++ budget_app/tests/test_forms.py
@@ -1,4 +1,5 @@
 import pytest
+from django.urls import reverse
 
 from budget_app.models import Contractors
 
@@ -25,31 +26,31 @@ def test_with_authenticated_client_add_contractor(client, django_user_model):
     assert Contractors.objects.count() == 1
     # assert len(Contractors.objects.all()) == 1
 
-# @pytest.mark.django_db
-# def test_with_authenticated_client_add_contractor(client, django_user_model):
-#     username = "user1"
-#     password = "bar"
-#     user = django_user_model.objects.create_user(username=username, password=password)
-#     # Use this:
-#     client.force_login(user)
-#     # Or this:
-#     # client.login(username=username, password=password)
-#     response = client.get('/contractors/')
-#     assert response.status_code == 200
-#
-#     response = client.post(reverse("contractors-view"), {
-#         'contractor_name': 'Company X',
-#         'city': 'City X',
-#         'zip_code': '00-000',
-#         'street_address': 'Street X',
-#     })
-#
-#     assert response.status_code == 302
-#     assert len(Contractors.objects.all()) == 1
-#
-#     contractor = Contractors.objects.first()
-#
-#     assert contractor.contractor_name == 'Company X'
-#     assert contractor.city == 'City X'
-#     assert contractor.zip_code == '00-000'
-#     assert contractor.street_address == 'Street X'
+@pytest.mark.django_db
+def test_with_authenticated_client_add_contractor(client, django_user_model):
+    username = "user1"
+    password = "bar"
+    user = django_user_model.objects.create_user(username=username, password=password)
+    # Use this:
+    client.force_login(user)
+    # Or this:
+    # client.login(username=username, password=password)
+    response = client.get('/contractors/')
+    assert response.status_code == 200
+
+    response = client.post(reverse("contractors-view"), {
+        'contractor_name': 'Company X',
+        'city': 'City X',
+        'zip_code': '00-000',
+        'street_address': 'Street X',
+    })
+
+    assert response.status_code == 302
+    assert len(Contractors.objects.all()) == 1
+
+    contractor = Contractors.objects.first()
+
+    assert contractor.contractor_name == 'Company X'
+    assert contractor.city == 'City X'
+    assert contractor.zip_code == '00-000'
+    assert contractor.street_address == 'Street X'
